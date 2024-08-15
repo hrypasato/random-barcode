b8236e5b238cad0e5893dac916b535280350ef7f
henry quinde <hrypasato@gmail.com>
2024-08-15 14:40:29 -0500

    fix. hi

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def main():
             st.success("Códigos de barras generados exitosamente. Haz clic en el enlace para descargar.")
 
             # Mostrar la tabla con los datos generados
-            df = pd.DataFrame(datos, columns=["Nombre del Producto", "Código EAN-13"], hi)
+            df = pd.DataFrame(datos, columns=["Nombre del Producto", "Código EAN-13"])
             st.subheader("Códigos EAN-13 Generados:")
             st.dataFrame(df.style.hide(), hide_index=True)
 
