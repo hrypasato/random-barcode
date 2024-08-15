54b27108657b4205faea62f07d6ae840775429cc
henry quinde <hrypasato@gmail.com>
2024-08-15 13:24:11 -0500

    feat: add table

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import csv
 import io
 import zipfile
 import base64
+import pandas as pd
 
 def generar_ean13_aleatorio():
     """Genera un número EAN-13 aleatorio válido (12 dígitos + dígito de control)"""
@@ -21,6 +22,7 @@ def generar_codigos_de_barras(productos):
     # Crear un archivo ZIP en memoria
     zip_buffer = io.BytesIO()
     with zipfile.ZipFile(zip_buffer, 'a', zipfile.ZIP_DEFLATED, False) as zip_file:
+        datos = []
         for producto in productos:
             ean = generar_ean13_aleatorio()
 
@@ -34,15 +36,16 @@ def generar_codigos_de_barras(productos):
             # Añadir la imagen al archivo ZIP
             zip_file.writestr(f"{producto}.png", img_buffer.getvalue())
 
-            # Agregar datos al archivo CSV
+            # Agregar datos al archivo CSV y a la lista de datos
             csv_writer.writerow([producto, ean])
+            datos.append({"Nombre del Producto": producto, "Código EAN-13": ean})
 
     # Añadir el archivo CSV al ZIP
     zip_buffer.seek(0)
     with zipfile.ZipFile(zip_buffer, 'a', zipfile.ZIP_DEFLATED, False) as zip_file:
         zip_file.writestr("productos_con_codigos_de_barras.csv", csv_buffer.getvalue())
 
-    return zip_buffer.getvalue()
+    return zip_buffer.getvalue(), datos
 
 def main():
     st.title("Generador de Códigos de Barras EAN-13")
@@ -55,7 +58,7 @@ def main():
 
     if st.button("Generar Códigos de Barras"):
         if productos:
-            zip_file = generar_codigos_de_barras(productos)
+            zip_file, datos = generar_codigos_de_barras(productos)
             
             # Crear un enlace de descarga para el archivo ZIP
             b64 = base64.b64encode(zip_file).decode()
@@ -63,6 +66,11 @@ def main():
             st.markdown(href, unsafe_allow_html=True)
             
             st.success("Códigos de barras generados exitosamente. Haz clic en el enlace para descargar.")
+
+            # Mostrar la tabla con los datos generados
+            df = pd.DataFrame(datos)
+            st.subheader("Códigos EAN-13 Generados:")
+            st.table(df)
         else:
             st.error("Por favor, ingrese al menos un nombre de producto.")
 
